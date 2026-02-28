59a85437b75ac67ab3ec360b3a9b2527260577ea
Daniel Preisz <61291434+DanyPreisz@users.noreply.github.com>
2026-02-28 14:22:46 -0300

    Add files via upload

---
+++ 1-variables/01-comentarios.js
@@ -0,0 +1,8 @@
+
+var x = 5; // Comentario de una línea
+
+/* Este es un
+comentario
+de varias
+líneas
+*/
---
+++ 1-variables/variables.js
@@ -0,0 +1,43 @@
+console.log("Hola Mundo!"); //Hola Mundo en JavaScript
+let hello = "Hello"
+let world = "World"
+
+// Message equals "Hello World"
+let message = hello + " " + world
+console.log(message)
+
+let x = 51 // Comentario de una línea
+let y = 6
+let result = x + y
+console.log(result)
+
+/* Este es un
+comentario
+de varias
+líneas
+*/
+
+// var
+
+var helloWorld = "¡Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "¡Hola, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3 = "¡Hola, JavaScript 3!"
+console.log(helloWorld3)
+
+// Error
+// helloWorld3 = "¡Hola de nuevo, JavaScript 2!"
+// console.log(helloWorld3)
